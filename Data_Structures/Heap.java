17a1250deab25cdff1b2dc54c82768755df782d4
thomas1242 <thomaspalmer121@gmail.com>
2017-08-08 21:51:49 -0400

    Heap

--- Data_Structures/Heap.java
+++ Data_Structures/Heap.java
@@ -1,32 +1,30 @@
 public class Heap {
-    public static void main(String[] args) {
+    
+    private int[] arr;
+    private int size;
 
-    	Heap heap = new Heap();
-    	heap.insert(5);
-    	heap.insert(7);
-    	heap.insert(1);
-        heap.insert(3);
-        heap.insert(2);
-    	heap.print();
-
-        while( !heap.isEmpty() ) 
-            System.out.println(heap.remove());
-        
-        // heap.print();
-
-    }
-
-    int[] arr;
-    int size;
     public Heap() {
     	arr = new int[8];
-    	size = 0;
+    }
+
+    int getLeft(int index) {
+        return index * 2;
+    }
+    int getRight(int index) {
+        return index * 2 + 1;
+    }
+    int getParent(int index) {
+        return index / 2;
+    }
+
+    boolean isEmpty() {
+        return size == 0;
     }
 
     void insert(int val) {
-    	arr[++size] = val; // bubble up
+    	arr[++size] = val; 
     	int index = size;
-    	while( index > 0 && arr[index] < arr[ getParent(index) ] ) {
+    	while( index > 0 && arr[index] < arr[ getParent(index) ] ) {   // bubble up
     		int temp = arr[ getParent(index) ];
             arr[ getParent(index) ] = arr[index];
             arr[index] = temp;
@@ -35,13 +33,11 @@ public class Heap {
     }
 
     int remove() {
-        print();
-
         int res = arr[1];
         arr[1] = arr[size--];
-        int index = 1;
 
-        while(index <= size && ( arr[index] > arr[getLeft(index)] || arr[index] > arr[getRight(index)] )) {
+        int index = 1;
+        while(index < size && ( arr[index] > arr[getLeft(index)] || arr[index] > arr[getRight(index)] )) {     // bubble down
             if(arr[getLeft(index)] < arr[getRight(index)]) {
                 int temp = arr[index];
                 arr[index] = arr[getLeft(index)];
@@ -55,19 +51,8 @@ public class Heap {
                 index = getRight(index);
             }
         }
-                print();
 
         return res;
-    }
-
-    int getLeft(int index) {
-    	return index * 2;
-    }
-    int getRight(int index) {
-    	return index * 2 + 1;
-    }
-    int getParent(int index) {
-    	return index / 2;
     }
 
     void print() {
@@ -76,13 +61,23 @@ public class Heap {
         System.out.println();
     }
 
-    boolean isEmpty() {
-        return size == 0;
+    public static void main(String[] args) {
+
+        Heap heap = new Heap();
+        heap.insert(5);
+        heap.insert(7);
+        heap.insert(1);
+        heap.insert(3);
+        heap.insert(2);
+        heap.print();
+
+        while( !heap.isEmpty() ) 
+            System.out.println(heap.remove());
     }
 
 
-  //  	    1
- 	//   2     3
+    //       1
+ 	//   2      3
  	// 4   5  6   7 
 
 
