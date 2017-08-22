e8a3af36c5646b3a51b2719a55e0621069a32dd7
Thomas L. Palmer <tpalmer@lin114-09>
2017-08-22 11:10:22 -0400

    BST

--- C++/BinarySearchTree/BinarySearchTree.h
+++ C++/BinarySearchTree/BinarySearchTree.h
@@ -1,16 +1,34 @@
 #ifndef BST_H
 #define BST_H
 
+struct Node {
+	int data;
+	Node * left;
+	Node * right;
+	Node() {
+		left = NULL;
+		right = NULL;
+	}
+	Node(int d) {
+		data = d;
+		left = NULL;
+		right = NULL;	
+	}
+
+};
+
 class BinarySearchTree {
 
 	public:
 		BinarySearchTree();
 		~BinarySearchTree();
 	private:
+		Node * root;
 
 };
 
 BinarySearchTree::BinarySearchTree() {
+	root = new Node();
 }
 
 BinarySearchTree::~BinarySearchTree() {
