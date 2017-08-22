0898bfe5575a5e2467ae3c1747f06db2d4fb3b19
Thomas L. Palmer <tpalmer@lin114-08>
2017-08-21 20:22:14 -0400

    Trie

--- C++/Trie/Main.cpp
+++ C++/Trie/Main.cpp
@@ -5,5 +5,23 @@ int main() {
 
 	Trie * trie = new Trie();
 
+	trie->insertWord( "tea" );
+	trie->insertWord( "ted" );
+	trie->insertWord( "to" );
+	trie->insertWord( "ten" );
+	trie->insertWord( "tent" );
+
+	trie->printAll();
+
+	bool res = true;
+
+	res &= !trie->contains("te");
+	res &= trie->contains("tea");
+
+	if(res)	std::cout << "\033[1;32mPASS\033[0m" << std::endl;
+	else 	std::cout << "\033[1;31mFAIL\033[0m" << std::endl;
+
+	trie->~Trie();
+
 	return 0;
 }
--- C++/Trie/Trie.h
+++ C++/Trie/Trie.h
@@ -1,5 +1,7 @@
 #ifndef TRIE_H
 #define TRIE_H
+#include <string>
+#include <iostream>
 
 struct Node {
 	Node ** children;	// pointer to array of child nodes
@@ -21,18 +23,86 @@ class Trie {
 	public:
 		Trie();
 		~Trie();
+		void insertWord(const std::string & word);
+		bool contains(const std::string & word) const;
+		void printAll() const;
 
 	private:
 		Node * root;
-
-
+		void printAll(const Node * node, std::string prefix) const;
 };
 
 Trie::Trie() {
 	root = new Node(' ');
 }
 
-Trie::~Trie() {
+void Trie::printAll() const {
+	printAll(root, "");
+}
+
+void Trie::printAll(const Node * node, std::string prefix) const {
+	if(node == NULL)
+		return;
+
+	prefix = prefix + node->c;	// update prefix associated with this node
+
+	if(node->isWord == true)
+		std::cout << prefix << std::endl;
+
+	for(int i = 0; i < 26; i++) 
+		printAll(node->children[i], prefix);
+}
+
+
+
+void Trie::insertWord(const std::string & word) {
+
+	Node * temp = root;
+	for(int i = 0; i < word.length(); i++) {
+		char c = word.at(i);
+		int index = c - 'a';	
+
+		if(temp->children[index] == NULL)
+			temp->children[index] = new Node( c );
+		temp = temp->children[index];
+	}
+
+	temp->isWord = true;
+}
+
+
+bool Trie::contains(const std::string & word) const {
+
+	Node * temp = root;
+	for(int i = 0; i < word.length(); i++) {
+		char c = word.at(i);
+		int index = c - 'a';	
+
+		if(temp->children[index] == NULL)
+			return false;
+		temp = temp->children[index];
+	}
+
+	if(temp->isWord == true)
+		return true;
+	else
+		return false;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Trie::~Trie() {	// TODO : visit each Node and call its destructor
 }
 
 
