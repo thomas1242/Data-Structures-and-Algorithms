ec61a951fdaba6d0b272c246b96f34cbcb85d146
Thomas L. Palmer <tpalmer@lin114-09>
2017-08-22 11:06:57 -0400

    BST

---
+++ C++/BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,19 @@
+#ifndef BST_H
+#define BST_H
+
+class BinarySearchTree {
+
+	public:
+		BinarySearchTree();
+		~BinarySearchTree();
+	private:
+
+};
+
+BinarySearchTree::BinarySearchTree() {
+}
+
+BinarySearchTree::~BinarySearchTree() {
+}
+
+#endif
---
+++ C++/BinarySearchTree/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "BinarySearchTree.h"
+
+int main() {
+
+	BinarySearchTree * bst = new BinarySearchTree();	
+
+	bool res = true;
+	
+	if(res)	std::cout << "\033[1;32mPASS\033[0m" << std::endl;
+	else 	std::cout << "\033[1;31mFAIL\033[0m" << std::endl;
+
+	bst->~BinarySearchTree();
+
+	return 0;
+}
+
