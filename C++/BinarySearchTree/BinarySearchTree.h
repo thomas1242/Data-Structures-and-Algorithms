41ec387ee356a7c28bb72c9955c8d7f23883046e
Thomas L. Palmer <tpalmer@lin116-06>
2017-08-22 21:59:07 -0400

    add destructor, use nullptr instead of NULL

--- C++/BinarySearchTree/BinarySearchTree.h
+++ C++/BinarySearchTree/BinarySearchTree.h
@@ -1,20 +1,13 @@
 #ifndef BST_H
 #define BST_H
-
 #include <iostream>
 
 struct Node {
 	int data;
-	Node * left;
-	Node * right;
-	Node() {
-		left = NULL;
-		right = NULL;
-	}
-	Node(int d) {
-		data = d;
-		left = NULL;
-		right = NULL;	
+	Node * left = nullptr;
+	Node * right = nullptr;
+	Node(int data) {
+		this->data = data;
 	}
 };
 
@@ -24,49 +17,168 @@ class BinarySearchTree {
 		BinarySearchTree();
 		~BinarySearchTree();
 		void insert(const int & data);
+		void remove(const int & data);
+		bool contains(const int & data) const;
+		int size() const;
 		void inorder() const;
+		void preorder() const;
+		void postorder() const;
 	private:
 		Node * root;
-		Node * insert(Node * node, const int & data);
+		Node * insert(Node *& node, const int & data);
+		Node * remove(Node *& node, const int & data);
+		bool contains(Node * node, const int & data) const;
+		int getMin(const Node * node) const;
+		int size(const Node * node) const;
+		void deleteTree(Node *& node);
 		void inorder(const Node * node) const;
+		void preorder(const Node * node) const;
+		void postorder(const Node * node) const;
 
 };
+
+BinarySearchTree::BinarySearchTree() {
+	root = nullptr;
+}
+
+BinarySearchTree::~BinarySearchTree() {
+	deleteTree(root);
+}
+
+void BinarySearchTree::deleteTree(Node *& node) {
+	if(node == nullptr)
+		return;
+		
+	Node * left = node->left;
+	Node * right = node->right;
+	
+	delete node;
+	node = nullptr;
+	
+	deleteTree(left);
+	deleteTree(right);
+}
+
+bool BinarySearchTree::contains(const int & data) const {
+	return contains(root, data);
+}
+
+bool BinarySearchTree::contains(Node * node, const int & data) const {
+	if(node == nullptr)
+		return false;
+			
+	if(data < node->data) 
+		return contains(node->left, data);
+	else if (data > node->data) 
+		return contains(node->right, data);
+	else
+		return true;
+}
 
 void BinarySearchTree::insert(const int & data) {
 	root = insert(root, data);
 }
 
-Node * BinarySearchTree::insert(Node * node, const int & data) {
-	if(node == NULL)
+Node * BinarySearchTree::insert(Node *& node, const int & data) {
+	if(node == nullptr)
 		return new Node(data);
-	
-	if(data < node->data)
-		root->left = insert(root->left, data);
-	else 
-		root->right = insert(root->right, data);
-
-	return node;
+			
+	if(data < node->data) 
+		node->left = insert(node->left, data);
+	else if (data > node->data) 
+		node->right = insert(node->right, data);
+		
+	return node;		// return updated node
 }
 
-BinarySearchTree::BinarySearchTree() {
-	root = NULL;
+void BinarySearchTree::remove(const int & data) {
+	root = remove(root, data);
 }
 
-BinarySearchTree::~BinarySearchTree() {
+Node * BinarySearchTree::remove(Node *& node, const int & data) {
+	if(node == nullptr)	// data not found
+		return nullptr;	
+			
+	if(data < node->data) 
+		node->left = remove(node->left, data);
+	else if (data > node->data) 
+		node->right = remove(node->right, data);
+	else { 				
+		if(node->left == nullptr)
+			return node->right;
+		else if(node->right == nullptr)
+			return node->left;
+		else {
+			int min = getMin(node->right);
+			node->right = remove(node->right, min);
+			node->data = min;
+		}
+	}
+		
+	return node;		// return updated node
+}
+
+int BinarySearchTree::getMin(const Node * node) const {
+		if(node->left == nullptr)
+			return node->data;
+		else
+			return getMin(node->left);
 }
 
 void BinarySearchTree::inorder() const {
+	std::cout << "Inorder traversal: ";
 	inorder(root);
+	std::cout << std::endl;
 }
 
-
 void BinarySearchTree::inorder(const Node * node) const {
-	if(node == NULL)
+	if(node == nullptr)
 		return;
 
 	inorder(node->left);
-	std::cout << node->data << std::endl;
+	std::cout << node->data << " ";
 	inorder(node->right);
+}
+
+void BinarySearchTree::preorder() const {
+	std::cout << "Preorder traversal: ";
+	preorder(root);
+	std::cout << std::endl;
+}
+
+void BinarySearchTree::preorder(const Node * node) const {
+	if(node == nullptr)
+		return;
+
+	std::cout << node->data << " ";
+	preorder(node->left);
+	preorder(node->right);
+}
+
+void BinarySearchTree::postorder() const {
+	std::cout << "Postorder traversal: ";
+	postorder(root);
+	std::cout << std::endl;
+}
+
+void BinarySearchTree::postorder(const Node * node) const {
+	if(node == nullptr)
+		return;
+
+	postorder(node->left);
+	postorder(node->right);
+	std::cout << node->data << " ";
+}
+
+int BinarySearchTree::size() const {
+	return size(root);
+}
+
+int BinarySearchTree::size(const Node * node) const {
+	if(node == nullptr)
+		return 0;
+	else
+		return 1 + size(node->left) + size(node->right);
 }
 
 #endif
--- C++/BinarySearchTree/Main.cpp
+++ C++/BinarySearchTree/Main.cpp
@@ -4,22 +4,41 @@
 int main() {
 
 	BinarySearchTree * bst = new BinarySearchTree();	
+	
+	bool res = true;			// test BST API
 
 	bst->insert(1);
-//	bst->insert(3);
-//	bst->insert(5);
-//	bst->insert(7);
-//	bst->insert(10);
-
+	bst->insert(10);
+	bst->insert(-5);
+	bst->insert(-7);
+	bst->insert(3);
+	
 	bst->inorder();
+	bst->preorder();
+	bst->postorder();
+				
+	res &= bst->contains(1);
+	res &= bst->contains(10);
+	res &= bst->contains(-5);
+	res &= bst->contains(-7);	
 
-	bool res = true;
+	res &= bst->size() == 5;
+	bst->remove(1);
+	bst->remove(10);
+	bst->remove(-5);
+	bst->remove(-7);
+	res &= bst->size() == 1;
+
+	res &= !bst->contains(1);
+	res &= !bst->contains(10);
+	res &= !bst->contains(-5);
+	res &= !bst->contains(-7);	
 	
 	if(res)	std::cout << "\033[1;32mPASS\033[0m" << std::endl;
 	else 	std::cout << "\033[1;31mFAIL\033[0m" << std::endl;
 
 	bst->~BinarySearchTree();
-
+	
 	return 0;
 }
 
