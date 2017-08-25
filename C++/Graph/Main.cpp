1d838b0872db8132aa36e8ed00f9c5256bcb7a50
Thomas L. Palmer <tpalmer@lin114-09>
2017-08-25 15:56:16 -0400

    test DFS

--- C++/Graph/Main.cpp
+++ C++/Graph/Main.cpp
@@ -3,34 +3,30 @@
 
 int main() {
 				
-	Graph<int> * graph1 = new Graph<int>();	
-	graph1->addEdge(1, 5);
-	graph1->addEdge(1, 3);
-	graph1->addEdge(1, 9);
-	graph1->addEdge(5, 9);
-	graph1->addEdge(9, 5);
-	graph1->addEdge(9, 15);
-	graph1->addEdge(15, 20);
+	Graph<int> graph1;	
+	graph1.addEdge(1, 5);
+	graph1.addEdge(1, 3);
+	graph1.addEdge(1, 9);
+	graph1.addEdge(5, 9);
+	graph1.addEdge(9, 5);
+	graph1.addEdge(9, 15);
+	graph1.addEdge(15, 20);
 	
-	Graph<const char *> * graph2 = new Graph<const char *>();	
-	graph2->addEdge("one", "five");
-	graph2->addEdge("one", "three");
-	graph2->addEdge("one", "nine");
-	graph2->addEdge("five", "nine");
-	graph2->addEdge("nine", "five");
-	graph2->addEdge("nine", "fifteen");
-	graph2->addEdge("fifteen", "twenty");
+	Graph<const char *> graph2;	
+	graph2.addEdge("one", "five");
+	graph2.addEdge("one", "three");
+	graph2.addEdge("one", "nine");
+	graph2.addEdge("five", "nine");
+	graph2.addEdge("nine", "five");
+	graph2.addEdge("nine", "fifteen");
+	graph2.addEdge("fifteen", "twenty");
 
 	bool res = true;
-
-	res &= graph1->DFS(1, 20);
-	res &= graph2->DFS("one", "twenty");
+	res &= graph1.DFS(1, 20);
+	res &= graph2.DFS("one", "twenty");
 	
 	if(res)	std::cout << "\033[1;32mPASS\033[0m" << std::endl;
 	else 	std::cout << "\033[1;31mFAIL\033[0m" << std::endl;
-	
-	graph1->~Graph();
-	graph2->~Graph();
 
 	return 0;
 }
