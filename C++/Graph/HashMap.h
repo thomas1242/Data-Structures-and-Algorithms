3934f21b53d4e035ab7fdaad5faf6e6cea193165
Thomas L. Palmer <tpalmer@lin116-02>
2017-08-25 20:17:23 -0400

    modify constructor

--- C++/Graph/HashMap.h
+++ C++/Graph/HashMap.h
@@ -12,11 +12,10 @@ class HashMap : public Map<K, V> {
 		K key;
 		V value;
 		Node * next;
-		Node() {}
-		Node(const K & k, const V & v) {
+		Node() : key(), value() {}
+		Node(const K& k, const V& v) {
 			key = k;	
 			value = v;
-			next = NULL;
 		}
 	};
 
--- C++/Graph/Main.cpp
+++ C++/Graph/Main.cpp
@@ -12,7 +12,7 @@ int main() {
 	graph1.addEdge(9, 15);
 	graph1.addEdge(15, 20);
 	
-	///*
+	/*
 	Graph<const char *> graph2;				// successfully runs on Linux, but not MacOS
 	graph2.addEdge("one", "five");
 	graph2.addEdge("one", "three");
@@ -21,7 +21,7 @@ int main() {
 	graph2.addEdge("nine", "five");
 	graph2.addEdge("nine", "fifteen");
 	graph2.addEdge("fifteen", "twenty");
-	//*/
+	*/
 	
 	/*
 	Graph<std::string> graph3;			// successfully runs on MacOS, but not Linux
@@ -35,14 +35,14 @@ int main() {
 	*/
 
 	bool res = true;
-	//res &= graph1.DFS(1, 20);
-	res &= graph2.DFS("one", "twenty");
+	res &= graph1.DFS(1, 20);
+	//res &= graph2.DFS("one", "twenty");
 
-	//graph1.clearVisited();
-	graph2.clearVisited();
+	graph1.clearVisited();
+	//graph2.clearVisited();
 
-	//res &= graph1.BFS(1, 20);
-	res &= graph2.BFS("one", "twenty");
+	res &= graph1.BFS(1, 20);
+	//res &= graph2.BFS("one", "twenty");
 	
 	if(res)	std::cout << "\033[1;32mPASS\033[0m" << std::endl;
 	else 	std::cout << "\033[1;31mFAIL\033[0m" << std::endl;
