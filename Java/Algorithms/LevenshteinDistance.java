b4b07a2151b5db22f9d97550f82edd90f4053fea
thomas1242 <thomaspalmer121@gmail.com>
2017-09-16 13:23:58 -0400

    Levenshtein distance

--- Java/Algorithms/LevenshteinDistance.java
+++ Java/Algorithms/LevenshteinDistance.java
@@ -1,5 +1,8 @@
 class LevenshteinDistance {
 
+	// The Levenshtein distance between two strings is the minimum number of single-character edits 
+	// 		  (insertions, deletions or substitutions) required to change one string into the other.
+	
 	static int getStringDifference(String s1, String s2) {
 		if(s1.length() == 0) return s2.length();	// insert/remove all of S2's chars
 		if(s2.length() == 0) return s1.length();	// insert/remove all of S1's chars	
@@ -7,7 +10,7 @@ class LevenshteinDistance {
 		int removeFromS1 = getStringDifference( s1.substring(0, s1.length() - 1), s2.substring(0, s2.length()) ) + 1; // remove/edit last char of S1
 		int removeFromS2 = getStringDifference( s1.substring(0, s1.length()), s2.substring(0, s2.length() - 1) ) + 1; // remove/edit last char of S2
 
-		int diff = (s1.charAt(s1.length() - 1)) == (s2.charAt(s2.length() - 1)) ? 0 : 1;	// remove/edit last char either string, if necessary, to make last chars the same
+		int diff = (s1.charAt(s1.length() - 1)) == (s2.charAt(s2.length() - 1)) ? 0 : 1;	// remove/edit last char of either string, if necessary, to make last chars the same
 		int removeFromBoth = getStringDifference( s1.substring(0, s1.length() - 1), s2.substring(0, s2.length() - 1) ) + diff;
 
 		return Math.min(removeFromBoth, Math.min(removeFromS1, removeFromS2));
