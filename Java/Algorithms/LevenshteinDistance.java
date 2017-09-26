d550ba5eef2399a3474ed7a59a940951fadf094b
thomas1242 <thomaspalmer121@gmail.com>
2017-09-26 11:14:38 -0400

    optimize with DP

--- Java/Algorithms/LevenshteinDistance.java
+++ Java/Algorithms/LevenshteinDistance.java
@@ -3,7 +3,7 @@ class LevenshteinDistance {
 	// The Levenshtein distance between two strings is the minimum number of single-character edits 
 	// 		  (insertions, deletions or substitutions) required to change one string into the other.
 	
-	static int getStringDifference(String s1, String s2) {
+	static int getStringDifference(String s1, String s2) {	   // O(3 ^ n)
 		int len1 = s1.length(), len2 = s2.length();
 
 		if(len1 == 0) return len2;	
@@ -19,9 +19,36 @@ class LevenshteinDistance {
 		return 1 + Math.min(editBoth, Math.min(editS1, editS2));
 	}
 
+	static int getStringDifference_dp(String s1, String s2) {	// O(n ^ 2)
+		int len1 = s1.length(), len2 = s2.length();
+
+		int[][] editDistance = new int[len1 + 1][len2 + 1];
+
+		for (int i = 0; i <= len1; i++) {
+			for (int j = 0; j <= len2; j++) {
+				if(i == 0)
+					editDistance[i][j] = j;
+				else if(j == 0)
+					editDistance[i][j] = i;
+				else if(s1.charAt(i - 1) == s2.charAt(j - 1))
+					editDistance[i][j] = editDistance[i - 1][j - 1];
+				else
+					editDistance[i][j] = 1 + min(editDistance[i - 1][j], editDistance[i][j - 1], editDistance[i-1][j-1]);
+			}
+		}
+
+		return editDistance[len1][len2];
+	}
+
+	static int min(int a, int b, int c) {
+		return Math.min(a, Math.min(b, c));
+	}
+
 	public static void main(String[] args) {
 		String s1 = "kitten";
 		String s2 = "sitting";
 		System.out.println( getStringDifference(s1, s2) );
+		System.out.println( getStringDifference_dp(s1, s2) );
+
 	}
 }
