163b21992de147361ee93e76ef97bb3829a5a0e2
thomas1242 <thomaspalmer121@gmail.com>
2018-02-10 18:47:45 -0500

    clean recursive solution

--- Java/Algorithms/LetterCombinationsOfPhoneNumber.java
+++ Java/Algorithms/LetterCombinationsOfPhoneNumber.java
@@ -3,8 +3,28 @@ import java.util.LinkedList;
 
 class LetterCombinationsOfPhoneNumber {
 
-	static List<String> letterCombinations(String digits) {
-        String[] dict = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    // recursive solution
+    static List<String> letterCombinations_1(String digits) {
+        List<String> combinations = new LinkedList<>();
+
+        recurse(digits, combinations, new StringBuilder());
+
+        return combinations;
+    }
+    
+    static void recurse(String digits,  List<String> letterCombinations, StringBuilder sb) {
+        String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        for(char c : map[Character.getNumericValue(digits.charAt(0))].toCharArray())
+            if(digits.length() == 1) 						// base case
+                letterCombinations.add(sb.toString() + c); 
+            else                     						// recursive step
+                recurse(digits.substring(1),  letterCombinations, new StringBuilder(sb.toString() + c));        
+    }
+
+    // iterative solution 
+	static List<String> letterCombinations_2(String digits) {
+        String[] map = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         
         List<String> q = new LinkedList<>();        
         q.add("");
@@ -12,8 +32,8 @@ class LetterCombinationsOfPhoneNumber {
         for(int i = 0; i < digits.length(); i++) {
             while(q.get(0).length() < i + 1) {
                 String curr = q.remove(0);
-                for(int j = 0; j < dict[ Character.getNumericValue(digits.charAt(i)) ].length(); j++) 
-                    q.add( curr +  dict[ Character.getNumericValue(digits.charAt(i)) ].charAt(j) );
+                for(int j = 0; j < map[ Character.getNumericValue(digits.charAt(i)) ].length(); j++) 
+                    q.add( curr +  map[ Character.getNumericValue(digits.charAt(i)) ].charAt(j) );
             }
         }
         
@@ -21,17 +41,11 @@ class LetterCombinationsOfPhoneNumber {
     }
 
 	public static void main(String[] args) {
-		String input = "23";
-		List<String> combos = letterCombinations( input );
+		List<String> combos = letterCombinations_1( "235" );
 
 		for(String combo : combos) 
 			System.out.print(combo + ", ");
 		System.out.println("\n");
 
-		input = "235";
-		combos = letterCombinations( input );
-
-		for(String combo : combos) 
-			System.out.print(combo + ", ");
 	}
 }
