baae7f2c7bff6856643f8b3633ace6cd310233aa
thomas1242 <tpalmer1243@gmail.com>
2018-05-02 18:15:13 -0400

    simple unweighted directed graph

--- Java/Data_Structures/Graph.java
+++ Java/Data_Structures/Graph.java
@@ -4,117 +4,98 @@ import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.Stack;
 
-public class Graph<T> {
+public class Graph<T> {	// simple unweighted directed graph
 
 	private static class Node<T> {
 		T id;
 		Node parent;
-		List<Node> adj;
+		List<Node<T>> adjNodes;
 		boolean visited;
 
 		public Node(T id) {
 			this.id = id;
-			adj = new LinkedList<>();
+			adjNodes = new LinkedList<>();
 		}
 	}
 
-	private HashMap<T, Node> graph;
+	private HashMap<T, Node<T>> graph;
 
 	public Graph() {
 		graph = new HashMap<>();
 	}
 
-	public void addEdge(T v1, T v2) {
-		if(graph.get(v1) == null) 
-			graph.put( v1, new Node(v1) );
-		if(graph.get(v2) == null) 
-			graph.put( v2, new Node(v2) );
+	public void addEdge(T v1, T v2) {	// add directed edge from v1 to v2
+		if(graph.get(v1) == null)  graph.put(v1, new Node<>(v1));
+		if(graph.get(v2) == null)  graph.put(v2, new Node<>(v2));
 
-		graph.get(v1).adj.add( graph.get(v2) );
+		graph.get(v1).adjNodes.add( graph.get(v2) );
 	}
 
-	public boolean DFS(T v1, T v2) {
-		if(v1 == v2)
-			return true;
-
-		graph.get(v1).visited = true;
-
-		for(Node n : (List<Node>)graph.get(v1).adj) 
-			if( !n.visited ) 
-				return DFS((T)n.id, v2);
-
-		return false;
+	public void DFS(T v1) {
+		DFS(graph.get(v1));
 	}
 
-	public boolean BFS(T v1, T v2) {
+	private void DFS(Node<T> node) {
+		node.visited = true;
 
-		Queue<Node> q = new LinkedList<Node>();
+		for(Node<T> adjNode : node.adjNodes) 
+			if(!adjNode.visited) 
+				DFS(adjNode);
+	}
 
-		Node start = graph.get(v1);
-		Node end   = graph.get(v2);
+	public void BFS(T v1) {
+		BFS(graph.get(v1));
+	}
 
-		q.add( start );
-		start.visited = true;
+	private void BFS(Node<T> node) {
+		Queue<Node<T>> q = new LinkedList<>();
+		
+		Node<T> startNode = node;
+		q.add(startNode);
+		startNode.visited = true;
 
 		while( !q.isEmpty() ) {
-			Node curr = q.poll();
-			for(Node n : (List<Node>)curr.adj) { 
-				if( !n.visited ) {
-					if(n == end) 
-						return true;
-					else {
-						q.add(n);
-						n.visited = true;
-						n.parent = curr;
-					}
+			Node<T> curr = q.poll();
+			for(Node<T> adjNode : curr.adjNodes) { 
+				if(!adjNode.visited) {
+					q.add(adjNode);
+					adjNode.visited = true;
 				}
 			}
 		}
-
-		return false;
 	}
 
-	public List<T> getMinPath(T start, T end) {
+	public void printMinPath(T start, T end) {
+		Queue<Node<T>> q = new LinkedList<>();
 
-		Queue<Node> q = new LinkedList<Node>();
-
-		Node startNode = graph.get(start);
-		Node endNode   = graph.get(end);
+		Node<T> startNode = graph.get(start);
+		Node<T> endNode   = graph.get(end);
 
 		q.add( startNode );
 		startNode.visited = true;
 
 		while( !q.isEmpty() ) {
-			Node curr = q.poll();
-			for(Node n : (List<Node>)curr.adj) { 
-				if( !n.visited ) {
-					if(n == endNode) {
+			Node<T> curr = q.poll();
+			for(Node<T> adjNode : curr.adjNodes) { 
+				if( !adjNode.visited ) {
+					if(adjNode == endNode) {
 						endNode.parent = curr;
-						List<T> startToEndPath = new LinkedList<>();
-						Node reverse = reverseList(endNode);
-						while(reverse != null) {
-							startToEndPath.add((T)reverse.id);
-							reverse = reverse.parent;
-						}
-						return startToEndPath;
+						printList( reverseList(endNode) );
 					}
 					else {
-						q.add(n);
-						n.visited = true;
-						n.parent = curr;
+						q.add(adjNode);
+						adjNode.visited = true;
+						adjNode.parent = curr;
 					}
 				}
 			}
 		}
-
-		return null;
 	}
 
-	private static Node reverseList(Node head) {
-        
-        Node curr = head;
-        Node prev = null;
-        Node next = null;
+	private Node<T> reverseList(Node<T> head) {
+        Node<T> curr = head;
+        Node<T> prev = null;
+        Node<T> next = null;
         
         while(curr != null) {
             next = curr.parent;
@@ -127,26 +108,30 @@ public class Graph<T> {
     }
 
 	public void clearVisited() {
-		for(Node n : graph.values()) n.visited = false;
+		for(Node<T> node : graph.values()) 
+			node.visited = false;
+	}
+
+	public void printList(Node<T> node) {
+		while(node != null) {
+			System.out.print(node.id + " ");
+			node = node.parent;
+		}
 	}
 
 	public static void main(String[] args) {
-
 		Graph<Integer> g = new Graph<>();
-
 		g.addEdge(1, 5);
-		g.addEdge(1, 3);
+		g.addEdge(1, 3);	
 		g.addEdge(1, 9);
 		g.addEdge(5, 9);
 		g.addEdge(9, 5);
 		g.addEdge(9, 15);
 		g.addEdge(15, 20);
-
 		System.out.print("Shortest path from " + 5 + " to " + 20 + " : ");
-		System.out.println(  g.getMinPath(5, 20)  );
+		g.printMinPath(5, 20);
 
 		Graph<String> g1 = new Graph<>();
-
 		g1.addEdge("one", "five");
 		g1.addEdge("one", "three");
 		g1.addEdge("one", "nine");
@@ -154,9 +139,7 @@ public class Graph<T> {
 		g1.addEdge("nine", "five");
 		g1.addEdge("nine", "fifteen");
 		g1.addEdge("fifteen", "twenty");
-
-		System.out.print("Shortest path from " + "five" + " to " + "twenty" + " : ");
-		System.out.println(  g1.getMinPath("five", "twenty")  );
+		System.out.print("\nShortest path from " + "five" + " to " + "twenty" + " : ");
+		g1.printMinPath("five", "twenty");
 	}
-
 }
