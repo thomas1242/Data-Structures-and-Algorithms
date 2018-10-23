e520bb6f9a8004c0fc96264f5e6d767e38b08c13
Thomas Palmer <thomaspalmer121@gmail.com>
2018-10-23 15:54:39 -0400

    Update HashMap.java

--- Java/Data_Structures/HashMap.java
+++ Java/Data_Structures/HashMap.java
@@ -9,8 +9,8 @@ public class HashMap<K, V> {
 		private V value;
 		private Node<K, V> next;
 		public Node(K key, V value) {
-				this.key = key;
-				this.value = value;
+			this.key = key;
+			this.value = value;
 		}
 	}
 
