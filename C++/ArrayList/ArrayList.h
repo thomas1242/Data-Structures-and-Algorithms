8db3223f4b0676e327a6ad6d8bfbfe12d597b1e5
thomas1242 <tpalmer1243@gmail.com>
2019-01-13 14:26:16 -0500

    add copy constructor, copy assignment, move constructor, move assignment

--- C++/ArrayList/ArrayList.h
+++ C++/ArrayList/ArrayList.h
@@ -5,59 +5,86 @@
 #include "LinearList.h"
 
 template<class T>
-void resize1DArray(T *& arr, int oldLength, int newLength) {
-	if(newLength < 0) 
-		throw std::runtime_error("new length must be >= 0");
+void resize1DArray(T *& oldArr, int oldLength, int newLength) {
+	if(newLength < 0)
+		throw std::length_error("new length must be >= 0");
+
 	T * newArr = new T[newLength];
 	for(int i = 0; i < oldLength; i++)
-		newArr[i] = arr[i];
-	delete[] arr;
-	arr = newArr;
+		newArr[i] = oldArr[i];
+	delete[] oldArr;
+	oldArr = newArr;
 }
 
 template <class T>
 class ArrayList : public LinearList<T> {
 
-	public: 
-		ArrayList() : ArrayList(10) {}	
-		ArrayList(int initCapacity);
-		~ArrayList();	
-		T& get(int index) const;	// inherited vritual functions
+	private:
+		T * arr = nullptr;
+		int listLength;
+		int arrayLength;
+
+	public:
+		ArrayList(int initCapacity = 0) : arr{new T[initCapacity]},			// default constructor
+										  listLength(initCapacity),
+		                                  arrayLength(initCapacity) {}
+
+		ArrayList(const ArrayList<T>& old) : arr{new T[old.arrayLength]},	// copy constructor
+											 listLength{old.listLength},
+											 arrayLength{old.arrayLength}
+		{
+            for(int i = 0; i < old.listLength; i++)  arr[i] = old.arr[i];
+		}
+
+		ArrayList<T>& operator=(const ArrayList<T>& old) {					// copy assignment
+		    delete[] arr;
+            arr = new T[old.arrayLength];
+            for(int i = 0; i < old.listLength; i++) arr[i] = old.arr[i];
+            listLength = old.listLength;
+            arrayLength = old.arrayLength;
+            return *this;
+		}
+
+		ArrayList(ArrayList<T>&& old) : arr{old.arr},						// move constructor
+										listLength{old.listLength},
+										arrayLength{old.arrayLength}
+		{
+			old.arr = nullptr;
+		}
+
+		ArrayList<T>& operator=(ArrayList<T>&& old) {						// move assignment
+			delete[] arr;
+            arr = old.arr;
+            listLength = old.listLength;
+            arrayLength = old.arrayLength;
+			old.arr = nullptr;
+			return *this;
+		}
+
+		~ArrayList() { delete[] arr; };									    // destructor
+
+		T& get(int index) const;											// inherited virtual functions
+		T& operator[](int i) { return get(i); }
 		void append(const T& theElement);
 		void insert(int index, const T& theElement);
 		void remove(int index);
-		bool isEmpty() const;	
-		int size() const;	
-		std::string to_string() const;	
+		std::string to_string() const;
+		bool isEmpty() const { return listLength == 0; };
+		int  size() const { return listLength; };
 
-	private:
-		T * arr;			// pointer to 1D array 
-		int listLength;
-		int arrayLength;
-		
 };
-
-template<class T>
-ArrayList<T>::ArrayList(int initCapacity) : listLength(0), arrayLength(initCapacity) {
-	arr = new T[initCapacity];
-}
-
-template<class T>
-ArrayList<T>::~ArrayList() {
-	delete [] arr;
-}
 
 template<class T>
 T& ArrayList<T>::get(int index) const {
 	if(index < 0 || index >= listLength)
-		throw std::runtime_error("ArrayList.get(): Invalid position");
+		throw std::out_of_range("ArrayList.get(): Invalid position");
 	return arr[index];
 }
 
 template<class T>
 void ArrayList<T>::insert(int index, const T& theElement) {
 	if(index < 0 || index > listLength)
-		throw std::runtime_error("ArrayList.insert(): Invalid position");
+		throw std::out_of_range("ArrayList.insert(): Invalid position");
 
 	if(listLength == arrayLength) {
 		resize1DArray(arr, arrayLength, arrayLength * 2);
@@ -73,7 +100,7 @@ void ArrayList<T>::insert(int index, const T& theElement) {
 template<class T>
 void ArrayList<T>::remove(int index) {
 	if(index < 0 || index >= listLength)
-		throw std::runtime_error("ArrayList.remove(): Invalid position");
+		throw std::out_of_range("ArrayList.remove(): Invalid position");
 	for(int i = index + 1; i < listLength; i++) 
 		arr[i - 1] = arr[i];
 	listLength--;
@@ -91,19 +118,9 @@ void ArrayList<T>::append(const T& theElement) {
 template<class T>
 std::string ArrayList<T>::to_string() const{
 	std::string res;
-	for(int i = 0; i < listLength; i++) 
-		res += to_string(arr[i]) + " ";
+	for(int i = 0; i < listLength; i++)
+		res += std::to_string(arr[i]) + " ";
 	return res + '\n';
-}
-
-template<class T>
-bool ArrayList<T>::isEmpty() const {
-	return listLength == 0;
-}
-
-template<class T>
-int ArrayList<T>::size() const {
-	return listLength;
 }
 
 #endif
--- C++/ArrayList/Test.cpp
+++ C++/ArrayList/Test.cpp
@@ -1,30 +1,44 @@
 #include <iostream>
+#include <string>
 #include "ArrayList.h"
 
+template <typename T>
+ArrayList<T> f(const ArrayList<T>& list) {
+	ArrayList<T> tmp(list);						// call copy constructor
+	return tmp;									// call move assignment
+}
+
+template <typename T>
+ArrayList<T> f1(ArrayList<T> list) {			// call copy constructor
+	ArrayList<T> tmp(list);						// call copy constructor
+	return tmp;									// call move assignment
+}
+
+
 int main() {
-	ArrayList<int> * list = new ArrayList<int>();
+	ArrayList<int> list1;
+	ArrayList<int> list2 = list1;	// call copy constructor
+
+	for(int n : {2, 4, 6, 8}) list1.append(n);
+	for(int n : {1, 3, 5, 7}) list2.append(n);
+
+	std::cout << "list1: " << list1.to_string()  << std::endl;
+	std::cout << "list2: " << list2.to_string() << std::endl;
+
+	list2 = f1(list2); 
 
 	bool res = true;
+	res &= list2.get(0) == 1;
+	res &= list2.get( list2.size() - 1 ) == 7;
 
-	list->append(1);
-	list->append(3);
-	list->append(5);
-	list->append(7);
-	
-	res &= list->get(0) == 1;
-	res &= list->get( list->size() - 1 ) == 7;
+	list2.remove(0);
+	res &= list2.get(0) == 3;
+	res &= list2.get(2) == 7;
 
-	list->remove(0);
-	res &= list->get(0) == 3;
-	res &= list->get(2) == 7;
-
-	list->insert(0, 15);
-	res &= list->get(0) == 15;
+	list2.insert(0, 15);
+	res &= list2.get(0) == 15;
 
 	if(res)	std::cout << "\033[1;32mPASS\033[0m" << std::endl;
 	else 	std::cout << "\033[1;31mFAIL\033[0m" << std::endl;
-
-	(*list).~ArrayList();
-	
 	return 0;
 }
